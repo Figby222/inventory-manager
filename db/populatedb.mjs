e781322341dae89cbd9badef6ea917b1adf41d65
Figby222 <figby222@outlook.com>
2024-09-06 03:50:00 -0700

    Define connection tables after referenced tables
    Alter houses table to set listing_agent_id as
    foreign key connecting to userss

--- db/populatedb.mjs
+++ db/populatedb.mjs
@@ -15,48 +15,55 @@ const SQL = `CREATE TABLE IF NOT EXISTS houses (
     state VARCHAR ( 30 ) NOT NULL,
     zip_code NUMERIC ( 5 ) CONSTRAINT zip_code_digit_check CHECK (zip_code BETWEEN 0 AND 99999) NOT NULL,
     country VARCHAR ( 30 ) DEFAULT 'United States',
-    listing_agent_id INTEGER REFERENCES users (id)
+    listing_agent_id INTEGER NOT NULL
 );
+
+
+CREATE TABLE IF NOT EXISTS amenities (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    amenity_name VARCHAR ( 255 ) NOT NULL
+    );
 
 CREATE TABLE IF NOT EXISTS amenities_connection (
     house_id INTEGER REFERENCES houses (id) NOT NULL,
     amenity_id INTEGER REFERENCES amenities (id) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS amenities (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    amenity_name VARCHAR ( 255 ) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS owners_connection (
-    house_id INTEGER REFERENCES houses (id) NOT NULL,
-    owner_id INTEGER REFERENCES users (id) NOT NULL
-);
 
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     username VARCHAR ( 30 ) UNIQUE NOT NULL,
     first_name VARCHAR ( 30 ),
     last_name VARCHAR ( 30 )
-);
+    );
 
+CREATE TABLE IF NOT EXISTS owners_connection (
+    house_id INTEGER REFERENCES houses (id) NOT NULL,
+    owner_id INTEGER REFERENCES users (id) NOT NULL
+);
+    
 CREATE TABLE IF NOT EXISTS images (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    house_id INTEGER REFERENCES houses (id) NOT NULL,
     image_href TEXT NOT NULL,
     view_index NUMERIC ( 3 ) CONSTRAINT positive_view_index CHECK (view_index >= 0) NOT NULL,
+    house_id INTEGER REFERENCES houses (id) NOT NULL,
     UNIQUE (house_id, view_index)
 );
 
-CREATE TABLE IF NOT EXISTS categories_connection (
-    house_id INTEGER REFERENCES houses (id) NOT NULL,
-    category_id INTEGER REFERENCES categories (id) NOT NULL
-);
 
 CREATE TABLE IF NOT EXISTS categories (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     category_name VARCHAR ( 30 ) NOT NULL
+    );
+    
+CREATE TABLE IF NOT EXISTS categories_connection (
+    house_id INTEGER REFERENCES houses (id),
+    category_id INTEGER REFERENCES categories (id)
 );
+
+ALTER TABLE houses 
+    ADD CONSTRAINT houses_listing_agent_fk FOREIGN KEY (listing_agent_id)
+    REFERENCES users (id);
 `
 
 async function main() {
