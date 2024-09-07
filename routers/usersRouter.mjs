b3f203f0603a34ce6e9899feaffe44c93621b08c
Figby222 <figby222@outlook.com>
2024-09-07 07:42:55 -0700

    Add routers for update & deleting user

--- routers/usersRouter.mjs
+++ routers/usersRouter.mjs
@@ -3,13 +3,21 @@ import * as usersController from "../controllers/usersController.mjs";
 
 const usersRouter = Router();
 
-usersRouter.post("/new", usersController.newUserGet);
-
 usersRouter.get("/search", usersController.usersListSearchGet);
 
 usersRouter.get("/:userId", usersController.userDetailsGet);
 
+usersRouter.get("/update/:userId", usersController.updateUserPageGet);
+
 usersRouter.get("/", usersController.usersListGet);
+
+
+usersRouter.post("/new", usersController.newUserGet);
+
+usersRouter.post("/update/:userId", usersController.updateUserPost);
+
+
+usersRouter.post("/delete/:userId", usersController.deleteUserPost);
 
 
 
