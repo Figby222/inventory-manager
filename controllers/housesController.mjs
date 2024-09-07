7aedcfe56b855230dcda04375874723aded9a19c
Figby222 <figby222@outlook.com>
2024-09-07 08:26:36 -0700

    Add housesListSearchGet() controller

--- controllers/housesController.mjs
+++ controllers/housesController.mjs
@@ -9,4 +9,14 @@ const houseDetailsGet = asyncHandler(async (req, res) => {
     }
 })
 
-export { houseDetailsGet }
+const housesListSearchGet = asyncHandler(async (req, res) => {
+    const houseList = [ { houseList: "blah blah blah" } ]
+    console.log(`house list search query ${req.query}`);
+
+    if (houseList.length === 0) {
+        res.status(404).send("error page");
+    }
+    res.send("houseList view");
+})
+
+export { houseDetailsGet, housesListSearchGet }
