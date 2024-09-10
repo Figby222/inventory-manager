614e2aae91b93ef60d8a01268f65dfa6ed980924
Figby222 <figby222@outlook.com>
2024-09-09 21:21:04 -0700

    Add createHouseFormValidator

--- controllers/housesController.mjs
+++ controllers/housesController.mjs
@@ -1,4 +1,48 @@
 import asyncHandler from "express-async-handler";
+import { query, body, validationResult } from "expres-validator";
+
+const createHouseFormValidator = [
+    body("title")
+        .notEmpty().withMessage("Title field must not be empty")
+        .isLength({ max: 30 }).withMessage("Title field must be less than 30 characters"),
+    body("price")
+        .notEmpty().withMessage("Price field must not be empty")
+        .isNumeric().withMessage("Price field must be a number")
+        .isInt({ min: 10000 }).withMessage("Price field must be at least $10,000"),
+    body("bedroomCount")
+        .notEmpty().withMessage("Bedrooms field must not be empty")
+        .isNumeric().withMessage("Bedrooms field must be a number")
+        .isFloat({ min: 0 }).withMessage("Bedrooms field must be above 0"),
+    body("bathroomCount")
+        .notEmpty().withMessage("Bathrooms field must not be empty")
+        .isNumeric().withMessage("Bathrooms field must be a number")
+        .isFloat({ min: 0 }).withMessage("Bathrooms field must be above 0"),
+    body("squareFootage")
+        .notEmpty().withMessage("Square Ft. field must not be empty")
+        .isNumeric().withMessage("Square Ft. field must be a number")
+        .isInt({ min: 0 }).withMessage("Square Ft. field must be above 0"),
+    body("houseNumber")
+        .notEmpty().withMessage("House Number field must not be empty")
+        .isNumeric().withMessage("House Number field must be a number")
+        .isInt({ min: 0 }).withMessage("House Number field must be above 0"),
+    body("streetName")
+        .notEmpty().withMessage("Street field must not be empty")
+        .isAlpha().withMessage("Street field must only contain alphabetical characters")
+        .isLength({ max: 30 }).withMessage("Street field must contain a maximum of 30 characters"),
+    body("cityName")
+        .notEmpty().withMessage("City field must not be empty")
+        .isAlpha().withMessage("City field must only contain alphabetical characters")
+        .isLength({ max: 30 }).withMessage("City field must contain a maximum of 30 characters"),
+    body("zipCode")
+        .notEmpty().withMessage("Zip/Postal Code field must not be empty")
+        .isNumeric().withMessage("Zip/Postal Code field must be a number")
+        .isInt({ min: 0, max: 99999 }).withMessage("Zip/Postal Code field must be between 00000 and 99999"),
+    body("countryName")
+        .optional({ values: "falsy" })
+        .isAlpha().withMessage("Country field must only contain alphabetical characters")
+        .isLength().withMessage("Country field must contain a maximum of 30 characters")
+        
+]
 
 const houseDetailsGet = asyncHandler(async (req, res) => {
     const houseDetails = { houseDetails: "blahblahblah" }
