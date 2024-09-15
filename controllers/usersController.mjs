a0b3176c1b849de5ca3473e12f86b4d6fa116f17
Figby222 <figby222@outlook.com>
2024-09-14 22:28:04 -0700

    Set user as request body in createUserPost()

--- controllers/usersController.mjs
+++ controllers/usersController.mjs
@@ -82,7 +82,13 @@ const createUserPost = [
     asyncHandler(async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            res.status(400).render("createUser", { title: "Create a User", errors: errors.errors });
+            res.status(400).render("createUser", { 
+                title: "Create a User", 
+                errors: errors.errors, 
+                user: { 
+                    ...req.body 
+                } 
+            });
             return;
         }
 
