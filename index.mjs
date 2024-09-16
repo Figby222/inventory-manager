a3538480d431d68aa3eae84ca7bf09d750fcbbca
Figby222 <figby222@outlook.com>
2024-09-15 19:31:33 -0700

    Add middleware to serve static files

--- index.mjs
+++ index.mjs
@@ -8,10 +8,12 @@ import "dotenv/config";
 
 const __dirname = import.meta.dirname;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(express.urlencoded({ extended: true }));
 
 app.use("/houses", housesRouter);
 
